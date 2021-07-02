606a0ef5312a531247acb2e1939550652983e976
indapp <indapp.info@gmail.com>
2021-07-02 13:44:51 +0200

    Geolocator & user position update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:zero_hunger/screens/report.dart';
 
@@ -12,6 +15,14 @@ class _HomePageState extends State<HomePage> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  Completer<GoogleMapController> _mapControllerCompleter = Completer();
+
+  @override
+  void initState() {
+    super.initState();
+    _centerPositionToUser();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -28,6 +39,8 @@ class _HomePageState extends State<HomePage> {
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
         initialCameraPosition: _kGooglePlex,
+        zoomControlsEnabled: false,
+        onMapCreated: _onMapCreated,
       );
 
   _buildFab(BuildContext context) => FloatingActionButton.extended(
@@ -36,11 +49,41 @@ class _HomePageState extends State<HomePage> {
         icon: Icon(Icons.flag),
       );
 
-  _buildReportPage(BuildContext context) => ReportPage();
+  Widget _buildReportPage(BuildContext context) => ReportPage();
+
+  void _centerPositionToUser() async {
+    final position = await _getCurrentePosition();
+
+    if (position != null) {
+      final mapController = await _mapControllerCompleter.future;
+
+      final latLng = LatLng(position.latitude, position.longitude);
+      final cameraUpdate = CameraUpdate.newLatLng(latLng);
+
+      await mapController.moveCamera(cameraUpdate);
+    }
+  }
 
   _onHelpPressed() {
     Navigator.of(context).push(
       MaterialPageRoute(builder: _buildReportPage),
     );
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    _mapControllerCompleter.complete(controller);
+  }
+
+  Future<Position?> _getCurrentePosition() async {
+    if (!(await Geolocator.isLocationServiceEnabled())) return null;
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) return null;
+    }
+    if (permission == LocationPermission.deniedForever) return null;
+
+    final position = Geolocator.getCurrentPosition();
+    return position;
   }
 }
