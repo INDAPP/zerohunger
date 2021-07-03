4bf53c0f9e765005a4046a5689ef3159d33ebc57
danieletozzi300726 <33416373+nymeria1744@users.noreply.github.com>
2021-07-03 04:08:06 +0200

    report page, show position

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,10 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
-
-int _currentValue = 2;
-DateTime selectedDate = DateTime.now();
-//late StreamSubscription<Position> _positionSubscription;
 
 class ReportPage extends StatefulWidget {
   @override
@@ -12,7 +11,24 @@ class ReportPage extends StatefulWidget {
 }
 
 class _ReportPageState extends State<ReportPage> {
+  int _currentValue = 2;
+  DateTime selectedDate = DateTime.now();
+  late StreamSubscription<Position> _positionSubscription;
+  Position? _currentPosition;
+
+
   @override
+  void initState(){
+    _positionSubscription =
+        Geolocator.getPositionStream().listen(_onPositionUpdate);
+  }
+
+  @override
+  void dispose() {
+    _positionSubscription.cancel();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) =>
       Scaffold(
         appBar: _buildAppBar(context),
@@ -26,61 +42,72 @@ class _ReportPageState extends State<ReportPage> {
   }
 
   Widget _buildBody(BuildContext context) {
-    return Container(
-      child: Column(children: [/*
-        Row(
-          children:[
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        child: Column(children: [
+          Text('Your position: $_currentPosition'),
 
-          ]
-        ),*/
-        SizedBox(height: 16,),
-        Text('When was your last meal?'),
-        SizedBox(height: 8,),
-        ElevatedButton(
-          onPressed: () {
-            // _pickDate();
-          },
-          child: Text("Pick Date"),
-        ),
-        SizedBox(height: 16,),
-        CheckboxListTile(
-          title: Text('Need Water?'),
-          activeColor: Colors.deepOrangeAccent,
-          value: timeDilation != 1.0,
-          onChanged: (bool? value) {
-            setState(() {
-              timeDilation = value! ? 2.0 : 1.0;
-            });
-          },
-        ),
-        SizedBox(height: 16,),
-        Text(
-          'Select the number of people needing food: $_currentValue',
-        ),
-        SizedBox(height: 8,),
-        NumberPicker(
-          minValue: 1,
-          maxValue: 20,
-          step: 1,
-          axis: Axis.horizontal,
-          itemHeight: 60,
-          value: _currentValue,
-          onChanged: (value) => setState(() => _currentValue = value),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: Colors.deepOrangeAccent),
+          SizedBox(height: 16,),
+
+          Text('When was your last meal?'),
+
+          SizedBox(height: 8,),
+
+          ElevatedButton(
+            onPressed: () {
+              _pickDate;
+            },
+            child: Text("Pick Date"),
           ),
-        ),
-        SizedBox(height: 16,),
-        TextButton(
-          child: Text("Send Request"),
-          onPressed: _sendRequest,
-        ),
-      ]),
+
+          SizedBox(height: 16,),
+
+          CheckboxListTile(
+            title: Text('Need Water?'),
+            activeColor: Colors.deepOrangeAccent,
+            value: timeDilation != 1.0,
+            onChanged: (bool? value) {
+              setState(() {
+                timeDilation = value! ? 2.0 : 1.0;
+              });
+            },
+          ),
+
+          SizedBox(height: 16,),
+
+          Text(
+            'Select the number of people needing food: $_currentValue',
+          ),
+
+          SizedBox(height: 8,),
+
+          NumberPicker(
+            minValue: 1,
+            maxValue: 20,
+            step: 1,
+            axis: Axis.horizontal,
+            itemHeight: 60,
+            value: _currentValue,
+            onChanged: (value) => setState(() => _currentValue = value),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: Colors.deepOrangeAccent),
+            ),
+          ),
+
+          SizedBox(height: 16,),
+
+          TextButton(
+            child: Text("Send Request"),
+            onPressed: _sendRequest,
+          ),
+        ]),
+      ),
     );
   }
 
-  _pickDate(BuildContext context) async {
+  void _pickDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: selectedDate,
@@ -98,5 +125,11 @@ class _ReportPageState extends State<ReportPage> {
     navigator.pop();
   }
 
+
+  void _onPositionUpdate(Position position) {
+    setState(() {
+      _currentPosition = position;
+    });
+  }
 }
 
